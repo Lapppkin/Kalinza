b0a6b9b757946e31f2d3d6b1a7be01f3fe947eb8
deadie <zaq12wsx>
2019-08-28 09:20:31 +0300

    [upd] удален код старого окна авторизации

--- local/templates/kalinzaru/include/modals.php
+++ local/templates/kalinzaru/include/modals.php
@@ -3,59 +3,6 @@
 use Deadie\Helper;
 
 ?>
-
-<div id="modal_1" class="modal-overlay">
-    <div class="modal">
-
-        <a class="close-modal">
-            <svg viewBox="0 0 20 20">
-                <path fill="#4ab8e6"
-                    d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
-            </svg>
-        </a><!-- close modal -->
-
-        <div class="modal-contents TOLKO_MAIN">
-            <table style="width: 100%; text-align: center;">
-                <tr>
-                    <td><img src="/2/images/Logo.svg" width="80%"/></td>
-                </tr>
-            </table>
-            <div style="height: 50px; width: 100%; clear: both;"></div>
-
-            <? $APPLICATION->IncludeComponent(
-                "bitrix:system.auth.authorize",
-                "template1",
-                array(
-                    "AUTH_RESULT"        => $APPLICATION->arAuthResult,
-                    "COMPONENT_TEMPLATE" => "template1",
-                ),
-                false
-            ); ?>
-
-            <div style="height: 5px; width: 100%; clear: both;"></div>
-            <div class="dr_set">
-                <h3>Вход или регистрация<br>через соцсети</h3>
-                <?php
-                $APPLICATION->IncludeComponent(
-                    "ulogin:auth",
-                    "",
-                    Array(
-                        "SEND_MAIL"   => "N",
-                        "SOCIAL_LINK" => "Y",
-                        "GROUP_ID"    => array("5"),
-                        "ULOGINID1"   => "",
-                        "ULOGINID2"   => "",
-                    )
-                );
-                ?>
-                <div style="height: 15px; width: 100%; clear: both;"></div>
-            </div>
-
-        </div>
-
-    </div>
-</div>
-
 <!--auth modal-->
 <div class="modal fade" tabindex="-1" role="dialog" id="modal--auth">
     <div class="modal-dialog" role="document">
