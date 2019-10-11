880920845e021630a1d376174c86c763355096f0
deadie <zaq12wsx>
2019-10-11 15:12:24 +0300

    [fix] required из папки local

--- local/php_interface/core/EventHandler.php
+++ local/php_interface/core/EventHandler.php
@@ -4,16 +4,18 @@ namespace core;
 
 class EventHandler
 {
+    /**
+     * Check 404 error.
+     */
     public static function Check404Error()
     {
         global $APPLICATION, $USER;
 
         if (\defined('ERROR_404') && ERROR_404 == 'Y' && !\defined('ADMIN_SECTION')) {
             $APPLICATION->RestartBuffer();
-
-            require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/templates/' . SITE_TEMPLATE_ID . '/header.php';
+            require $_SERVER['DOCUMENT_ROOT'] . '/local/templates/' . SITE_TEMPLATE_ID . '/header.php';
             require $_SERVER['DOCUMENT_ROOT'] . '/404.php';
-            require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/templates/' . SITE_TEMPLATE_ID . '/footer.php';
+            require $_SERVER['DOCUMENT_ROOT'] . '/local/templates/' . SITE_TEMPLATE_ID . '/footer.php';
         }
     }
 }
