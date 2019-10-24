fa2c6683ab728c7a6513c409732961e8143efae9
deadie <zaq12wsx>
2019-10-24 13:37:15 +0300

    [upd] блок с вызовом консультанта

--- include/consult.php
+++ include/consult.php
@@ -6,15 +6,15 @@
                     <h2 class="h1 consult-form-title">Не нашли, что искали?</h2>
                     <p>Напишите нам, и наши менеджеры проконсультируют<br>вас по вашему вопросу.</p>
                     <div>
-                        <form id="consult-form">
+                        <form id="consult-form" method="post" action="<?= SITE_DIR . 'include/mail/mail.php' ?>">
                             <?= bitrix_sessid_post() ?>
                             <div class="form-item">
                                 <label for="consult-name" hidden></label>
-                                <input type="text" name="NAME" id="consult-name" required placeholder="Ваше имя *">
+                                <input type="text" name="name" id="consult-name" required placeholder="Ваше имя *">
                             </div>
                             <div class="form-item">
                                 <label for="consult-phone" hidden></label>
-                                <input type="text" name="PHONE" id="consult-phone" required placeholder="Ваш телефон/email *">
+                                <input type="text" name="phone" id="consult-phone" required placeholder="Ваш телефон/email *">
                             </div>
                             <div class="form-item">
                                 <button type="submit" class="btn">Отправить</button>
