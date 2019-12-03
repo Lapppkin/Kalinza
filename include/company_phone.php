a4bc6e30be531f1c1d29317eeeb73034d3a9bc4a
deadie <zaq12wsx>
2019-12-03 21:34:54 +0300

    [fix] исправлениe вывода телефона

--- include/company_phone.php
+++ include/company_phone.php
@@ -4,5 +4,6 @@ use core\Helper;
 use core\Regionality;
 
 $phone = Regionality::getCurrentRegionPhoneMain();
+if (empty($phone)) $phone = '8 800 301-21-01';
 
 print Helper::parsePhone($phone, 'link');
